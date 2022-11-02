11a8ce339bf7b3db82252979786944853ed82565
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2022-11-02 11:42:26 +0100

    Detail View und style hinzugefügt

--- azubi_list/urls.py
+++ azubi_list/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, include
-from .views import Azubi_List
+from .views import Azubi_List, detail_page
 
 urlpatterns = [
-    path("", Azubi_List.as_view(), name='azubi_list')
+    path("", Azubi_List.as_view(), name='azubi_list'),
+    path('<int:id>', detail_page, name="detail"),
 ]
