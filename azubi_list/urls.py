13dabab3bf715cc2fb99fffc35c441e77f7f12d8
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2022-11-02 14:38:42 +0100

    detailview und listview auf einer Seite

--- azubi_list/urls.py
+++ azubi_list/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import Azubi_List, detail_page
+from .views import Azubi_List,detail_page
 
 urlpatterns = [
     path("", Azubi_List.as_view(), name='azubi_list'),
-    path('<int:id>', detail_page, name="detail"),
+    #path('<int:id>', detail_page, name="detail"),
+    path('<int:id>/', detail_page, name="detail")
 ]
