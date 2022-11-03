05b770842047584dc69208ab9d8bccbd5fcaac00
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2022-11-03 13:06:08 +0100

    System zum Filtern implementiert(ohne ui)

--- azubi_list/urls.py
+++ azubi_list/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import detail_page, homepage
 
 urlpatterns = [
-    path("", homepage, name='azubi_list'),
-    path('<int:id>/', detail_page, name="detail")
+    path("<str:filter>/", homepage, name='azubi_list'),
+    path('<str:filter>/<int:id>/', detail_page, name="detail"),
 ]
