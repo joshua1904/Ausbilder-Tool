f8214844ccc7b4430a819f3f08e8b0dae7db09c7
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2022-11-09 11:25:02 +0100

    suchfunktion dikka

--- azubi_list/views.py
+++ azubi_list/views.py
@@ -1,23 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from .models import Azubi, Profession
 from django.shortcuts import get_object_or_404, render
+from.forms import SearchForm
 # Create your views here.
 
+class failureObject():
+    first_name = "Keine Treffer"
+    id = 0
+
+    
 def detail_page(request, id, filter):
+    form = SearchForm()
     obj = get_object_or_404(Azubi, pk=id)
-    azubis = Azubi.objects.all()
+    azubis = Azubi.objects.order_by('-last_name', "first_name")
     professions = Profession.objects.all()
     final_list = get_filter(filter, azubis)
-    return render(request, "azubi_detail.html", {"obj": obj, "azubis": final_list, "professions": professions, "filter": filter})   
+
+    return render(request, "azubi_detail.html", {"obj": obj, "azubis": final_list, "professions": professions, "filter": filter, "form": form})   
 
 #Ruft die selbe seite wie Detail page auf, braucht aber keine obj id (Vlt geht das ja aber auch besser)
 def homepage(request, filter: str):
-    azubis = Azubi.objects.all()
+    azubis = Azubi.objects.order_by('-last_name', "first_name")
+    form = SearchForm()
     professions = Profession.objects.all()
     final_list = get_filter(filter, azubis)
+    if request.method == "POST":
+        search = request.POST['azubi_search_input']
+        final_list = get_search(final_list, search)   
     if len(final_list) > 0:
-        return render(request, "azubi_detail.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "filter": filter})
-    return render(request, "azubi_detail.html", {"azubis": azubis,"professions": professions, "filter": filter})
+        return render(request, "azubi_detail.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "filter": filter, "form": form})
+    return render(request, "azubi_detail.html", {"azubis": azubis,"professions": professions, "filter": filter, "form": form})
+
+def get_search(azubis: Azubi, search_str: str):
+    final_list = list(filter(lambda x: f"{x.first_name}{x.last_name}".__contains__(search_str), azubis))
+    if len(final_list) == 0:
+        print(final_list)
+        #faked Daten eines Azubis zur Fehleranzeige
+        final_list = [failureObject()]
+    return final_list
 
 def get_filter(profession_filter: str, azubis):
     final_list = list()
@@ -25,3 +45,7 @@ def get_filter(profession_filter: str, azubis):
     if profession_filter == "all":
         final_list = azubis
     return final_list
+
+
+
+    
