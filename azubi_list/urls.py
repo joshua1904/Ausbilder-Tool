5b3f442da2e999b0a3bdabe4db8e8ba99cd7e03c
Ahmed-RAS <ahmed.rashid@kraussmaffei.com>
2022-11-16 15:48:12 +0100

    customize site created

--- azubi_list/urls.py
+++ azubi_list/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import detail_page, homepage
+from .views import detail_page, homepage, customize
 
 urlpatterns = [
     path("<str:filter>/", homepage, name='azubi_list'),
     path('<str:filter>/<int:id>/', detail_page, name="detail"),
+    path("", customize, name="customize"),
     # path('frmtest/', form_name_view, name="form_name")
 ]
--- azubi_list/views.py
+++ azubi_list/views.py
@@ -20,8 +20,9 @@ def detail_page(request, filter, id):
     azubis = Azubi.objects.order_by('-last_name', "first_name")
     azubis = sort(azubis)
     professions = Profession.objects.all()
+    departments = Azubi.objects.values_list('department')
     final_list = get_filter(filter, azubis)
-    return render(request, "azubi_detail.html", {"obj": obj, "azubis": final_list, "professions": professions, "filter": filter, "form": form})   
+    return render(request, "azubi_detail.html", {"obj": obj, "azubis": final_list, "professions": professions, "departments": departments, "filter": filter, "form": form})   
 
 #Ruft die selbe seite wie Detail page auf, braucht aber keine obj id (Vlt geht das ja aber auch besser)
 def homepage(request, filter: str):
@@ -29,13 +30,14 @@ def homepage(request, filter: str):
     azubis = sort(azubis)
     form = SearchForm()
     professions = Profession.objects.all()
+    departments = Azubi.department
     final_list = get_filter(filter, azubis)
     if request.method == "POST":
         search = request.POST['azubi_search_input']
         final_list = get_search(final_list, search)   
     if len(final_list) > 0:
-        return render(request, "azubi_detail.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "filter": filter, "form": form})
-    return render(request, "azubi_detail.html", {"azubis": azubis,"professions": professions, "filter": filter, "form": form})
+        return render(request, "azubi_detail.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "departments": departments, "filter": filter, "form": form})
+    return render(request, "azubi_detail.html", {"azubis": azubis,"professions": professions, "departments": departments, "filter": filter, "form": form})
 
 def get_search(azubis: Azubi, search_str: str):
     final_list = list(filter(lambda x: f"{x.first_name}{x.last_name}".__contains__(search_str), azubis))
@@ -45,13 +47,23 @@ def get_search(azubis: Azubi, search_str: str):
         final_list = [failureObject()]
     return final_list
 
-def get_filter(profession_filter: str, azubis):
-    final_list = list()
-    final_list = list(filter(lambda x: x.topic.name == profession_filter, azubis))
-    if profession_filter == "all":
+def get_filter(filter_: str, azubis):
+    temp_list = list()
+    #if filter_ == "profession":
+    final_list = list(filter(lambda x: x.topic.name == filter_, azubis))
+    if filter_ == "department":
+        final_list = list(filter(lambda x: x.department.name == filter_, azubis))
+    if filter_ == "all":
         final_list = azubis
+    #mydata = Azubi.objects.filter_(topic=filter_).values()
+    #mydata = Azubi.objects.filter_(topic=filter_, id=2).values()
+    #final_list = list(mydata)
     return final_list
 
 def sort(azubis) -> list:  
     sorted_list =  sorted(azubis, key=operator.attrgetter('email'))
     return sorted_list
+
+def customize(request):
+    azubis = Azubi.objects.all()
+    return render(request, "customize.html", {"azubis", azubis})
