bdc4c708e73e97f04153ca687797ade376ebbb32
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2023-01-11 11:50:06 +0100

    code refactor

--- azubi_list/urls.py
+++ azubi_list/urls.py
@@ -1,9 +1,8 @@
 
 from django.urls import path
-from .views import detail_page, homepage, start_page, add_azubi, delete_azubi, delete_confirm, edit_azubi_data
+from .views import homepage, start_page, add_azubi, delete_azubi, delete_confirm, edit_azubi_data
 urlpatterns = [
-    path("<str:filter>/", homepage, name='azubi_list'),
-    path('<str:filter>/<int:id>/', detail_page, name="detail"),
+    path("<str:filter>/<int:year>/<int:id>/", homepage, name='azubi_list'),
     path('', start_page, name="start"),
     path('add/azubi', add_azubi, name="add"),
     path('delete/<int:id>/', delete_azubi, name='delete'),
--- azubi_list/views.py
+++ azubi_list/views.py
@@ -8,28 +8,17 @@ import operator
 class failureObject():
     first_name = "Keine Treffer"
     id = 0
-def detail_page(request, filter, id):
-    form = SearchForm()
-    obj = get_object_or_404(Azubi, pk=id)
-    azubis = Azubi.objects.order_by('-last_name', "first_name")
-    #azubis = sort(azubis)
-    professions = Profession.objects.all()
-    final_list = get_filter(filter, azubis)
-    if request.method == "POST":
-        if form.is_valid():
-            search = request.POST['azubi_search_input']
-            final_list = get_search(final_list, search)   
-            #delete = request.POST['delete']
-            #return delete_confirm(request, id)
-    return render(request, "azubi_detail_2.html", {"obj": obj, "azubis": final_list, "professions": professions, "filter": filter, "form": form})   
+
 
 #Ruft die selbe seite wie Detail page auf, braucht aber keine obj id (Vlt geht das ja aber auch besser)
-def homepage(request, filter: str):
+def homepage(request, filter: str, year: int, id):
     azubis = Azubi.objects.order_by("last_name")
-    azubis = sort(azubis)
+    #azubis = sort(azubis)
     form = SearchForm()
     professions = Profession.objects.all()
     final_list = get_filter(filter, azubis)
+    final_list = get_year_filter(year, final_list)
+    print(final_list)
     if request.method == "POST":
         try:
             search = request.POST['azubi_search_input']
@@ -38,8 +27,11 @@ def homepage(request, filter: str):
             pass
   
     if len(final_list) > 0:
-        return render(request, "azubi_detail_2.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "filter": filter, "form": form})
-    return render(request, "azubi_detail_2.html", {"azubis": azubis,"professions": professions, "filter": filter, "form": form})
+        if id == 0:
+            return render(request, "azubi_detail_2.html", {"azubis": final_list, "obj": final_list[0], "professions": professions, "filter": filter, "form": form, "year": year, "id":id})
+        obj = get_object_or_404(Azubi, pk=id)
+        return render(request, "azubi_detail_2.html", {"azubis": final_list, "obj": obj, "professions": professions, "filter": filter, "form": form, "year": year, "id":id})
+    return render(request, "azubi_detail_2.html", {"azubis": final_list,"professions": professions, "filter": filter, "form": form, "year": year, "id": id})
 
 def get_search(azubis: Azubi, search_str: str):
     final_list = list(filter(lambda x: f"{x.first_name}{x.last_name}".lower().__contains__(search_str.lower()), azubis))
@@ -55,12 +47,20 @@ def get_filter(profession_filter: str, azubis):
         final_list = azubis
     return final_list
 
+
+def get_year_filter(year_filter: int, azubis: list):
+    final_list = list()
+    final_list = list(filter(lambda x: x.year == year_filter, azubis))
+    if year_filter == 0:
+        final_list = azubis
+    return final_list
+
 def sort(azubis) -> list:  
     sorted_list =  sorted(azubis, key=operator.attrgetter('email'))
     return sorted_list
 
 def start_page(request):
-    return homepage(request, "all")
+    return homepage(request, "all", 0, 0)
 
 
 def add_azubi(request):
@@ -79,7 +79,6 @@ def add_azubi(request):
     return render(request, "add_azubi.html", context)
 
 def delete_confirm(request, id):
-    print("hi")
     filter = "all"
     azubi = get_object_or_404(Azubi, pk=id)
     context = {"azubi": azubi, "filter": filter}
@@ -87,7 +86,7 @@ def delete_confirm(request, id):
     if request.method == "POST":
         print("hi_2")
         azubi.delete()
-        return homepage(request, "all")
+        return homepage(request, "all", 0, 0)
     return render(request, "delete_confirm.html", context)
 
 def delete_azubi(request, id):
@@ -95,7 +94,7 @@ def delete_azubi(request, id):
     if request.method == 'POST':
         azubi.delete()
 
-    return homepage(request, "all")
+    return homepage(request, "all", 0, 0)
 
 
 def edit_azubi_data(request, id):
@@ -104,7 +103,7 @@ def edit_azubi_data(request, id):
         form = AzubiForm(request.POST, instance=data)
         if form.is_valid():
             form.save()
-            return detail_page(request, "all", id)
+            return homepage(request, "all", 0, id)
     else:
         form = AzubiForm(instance=data)
     return render(request, 'change_azubi_values.html', {'form': form})
