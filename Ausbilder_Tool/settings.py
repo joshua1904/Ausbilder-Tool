76861b28b9c7d1b054162504ed6b944efb190270
Joshua Tewes <joshua.tewes@kraussmaffei.com>
2023-01-13 13:45:14 +0100

    verbesserung und aufräümen

--- Ausbilder_Tool/settings.py
+++ Ausbilder_Tool/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "azubi_list.apps.AzubiListConfig",
-    "login",
 ]
 
 MIDDLEWARE = [
--- login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LoginView
-urlpatterns = [
-    path("authenticate/", LoginView.as_view(), name="login")
-]
